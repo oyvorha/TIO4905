b754f501da7e6731591c830b73fa79779ae5b917
oyvorsh <oyvorsh@stud.ntnu.no>
2019-10-21 09:59:56 +0200

    Add all constraints

--- Model/gurobi_model.py
+++ Model/gurobi_model.py
@@ -8,11 +8,11 @@ d = DynamicFileVariables()
 try:
     m = Model("Bicycle")
 
-    # ------ SETS -------------
+    # ------ SETS -----------------------------------------------------------------------------
     Stations = f.stations
     Vehicles = f.vehicles
 
-    # ------ FIXED PARAMETERS -------
+    # ------ FIXED PARAMETERS -----------------------------------------------------------------
     time_horizon = f.time_horizon
     vehicle_cap = f.vehicle_cap
     station_cap = f.station_cap
@@ -21,7 +21,7 @@ try:
     handling_time = f.handling_time
     M = f.M
 
-    # ------- DYNAMIC PARAMETERS ------
+    # ------- DYNAMIC PARAMETERS --------------------------------------------------------------
     start_stations = d.start_stations
     init_vehicle_load = d.init_vehicle_load
     init_station_load = d.init_station_load
@@ -32,21 +32,20 @@ try:
     incoming_rate = d.incoming_rate
     incoming_flat_rate = d.incoming_flat_rate
 
-    # ------ VARIABLES --------
+    # ------ VARIABLES -------------------------------------------------------------------------
     x = m.addVars({(i, j, v) for i in Stations
-                     for j in Stations for v in Vehicles}, vtype=GRB.BINARY, name="x")
-    t = m.addVars({i for i in Stations}, vtype=GRB.CONTINUOUS, name="t")
-    q = m.addVars({(i, v) for i in Stations for v in Vehicles}, vtype=GRB.INTEGER, name="q")
-    l_B = m.addVars({(i, v) for i in Stations for v in Vehicles},
-                   vtype=GRB.INTEGER, name="l_B")
-    l_F = m.addVars({i for i in Stations}, vtype=GRB.INTEGER, name="l_F")
-    l_V = m.addVars({(i, v) for i in Stations for v in Vehicles}, vtype=GRB.INTEGER, name="l_V")
-    s_I = m.addVars({i for i in Stations}, vtype=GRB.INTEGER, name="s_I")
-    s_V = m.addVars({v for v in Vehicles}, vtype=GRB.INTEGER, name="s_V")
-    v = m.addVars({i for i in Stations}, vtype=GRB.INTEGER, name="v")
-    d = m.addVars({i for i in Stations}, vtype=GRB.CONTINUOUS, name="d")
+                     for j in Stations for v in Vehicles}, vtype=GRB.BINARY, lb=0, name="x")
+    t = m.addVars({i for i in Stations}, vtype=GRB.CONTINUOUS, lb=0, name="t")
+    q = m.addVars({(i, v) for i in Stations for v in Vehicles}, vtype=GRB.INTEGER, lb=0, name="q")
+    l_B = m.addVars({i for i in Stations}, vtype=GRB.INTEGER, lb=0, name="l_B")
+    l_F = m.addVars({i for i in Stations}, vtype=GRB.INTEGER, lb=0, name="l_F")
+    l_V = m.addVars({(i, v) for i in Stations for v in Vehicles}, vtype=GRB.INTEGER, lb=0, name="l_V")
+    s_I = m.addVars({i for i in Stations}, vtype=GRB.INTEGER, lb=0, name="s_I")
+    s_V = m.addVars({v for v in Vehicles}, vtype=GRB.INTEGER, lb=0, name="s_V")
+    v_S = m.addVars({i for i in Stations}, vtype=GRB.INTEGER, lb=0, name="v")
+    d = m.addVars({i for i in Stations}, vtype=GRB.CONTINUOUS, lb=0, name="d")
 
-    # ------- CONSTRAINTS --------
+    # ------- FEASIBILITY CONSTRAINTS ----------------------------------------------------------
     # Routing constraints
     m.addConstrs(x.sum(start_stations[v], '*', v) == 1 for v in Vehicles)
     m.addConstrs(x.sum('*', Stations[-1], v) == 1 for v in Vehicles)
