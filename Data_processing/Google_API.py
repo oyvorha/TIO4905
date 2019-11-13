93e1b15f62708868e20ab916225348b435544cfb
oyvorsh <oyvorsh@stud.ntnu.no>
2019-11-13 16:40:20 +0100

    Add key as environment variable

--- Data_processing/Google_API.py
+++ Data_processing/Google_API.py
@@ -1,7 +1,8 @@
 import requests
+import os
 
 base = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial"
-key = "AIzaSyDT87e1XqA9v2iMsHPKzqELhmP5kRXbVTs"
+key = os.environ['KEY']
 
 
 def get_driving_time(origin_lat, origin_lon, dest_lat, dest_lon):
@@ -10,6 +11,3 @@ def get_driving_time(origin_lat, origin_lon, dest_lat, dest_lon):
     r = requests.get(base, params=parameters)
     data = r.json()
     return round(int(data['rows'][0]['elements'][0]['duration']['value']) / 60, 2)
-
-
-print(get_driving_time(63.432251821057207, 10.406996458768845, 63.432503742934855, 10.400386825203896))
