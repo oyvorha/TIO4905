9aa485342c58ad8e5aca86fb0c02137868369405
oyvorsh <oyvorsh@stud.ntnu.no>
2019-11-15 17:34:39 +0100

    Rename station variables

--- Input/station.py
+++ Input/station.py
@@ -1,12 +1,12 @@
 
 class Station:
 
-    def __init__(self, longitude, latitude, bat_load, flat_load, ideal, demand, incoming_rate, flat_rate):
+    def __init__(self, longitude, latitude, bat_load, flat_load, incoming_bat_rate, flat_rate, outgoing, demand):
         self.longitude = longitude
         self.latitude = latitude
         self.init_station_load = bat_load
         self.init_flat_station_load = flat_load
-        self.ideal_state = ideal
+        self.battery_rate = incoming_bat_rate
+        self.flat_rate = flat_rate
+        self.outgoing_rate = outgoing
         self.demand = demand
-        self.incoming_rate = incoming_rate
-        self.incoming_flat_rate = flat_rate
