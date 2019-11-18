1ba5912932c8dec8190aeacd16f687ca2caf5eaa
oyvorsh <oyvorsh@stud.ntnu.no>
2019-11-18 17:58:32 +0100

    Add objective value in excel sheet!

--- Output/save_output.py
+++ Output/save_output.py
@@ -8,7 +8,7 @@ def save_output(models, fixed, dynamics, station_obj):
     for i in range(len(models)):
         key = "solvable_instance_" + str(len(fixed[i].stations)) + '_' + str(len(fixed[i].vehicles))
         if key not in dataframes_dict.keys():
-            df = pd.DataFrame(columns=['Init #stations', 'No. of stations', 'No. of vehicles',
+            df = pd.DataFrame(columns=['Init #stations', 'No. of stations', 'No. of vehicles', 'Objective Value',
                                                          'Vehicle capacity', 'Station capacity', 'Time Horizon',
                                                          'Demand scenario', 'Solution time', 'Gap', 'weight violation',
                                                          'weight deviation', 'weight reward', 'reward weight dev',
@@ -18,10 +18,10 @@ def save_output(models, fixed, dynamics, station_obj):
             dataframes_dict[key] = df
 
         new_row = {'Init #stations': fixed[i].initial_size, 'No. of stations': len(fixed[i].stations)-2,
-                   'No. of vehicles': len(fixed[i].vehicles), 'Vehicle capacity': fixed[i].vehicle_cap[0],
-                   'Station capacity': fixed[i].station_cap[1], 'Time Horizon': fixed[i].time_horizon,
-                   'Demand scenario': fixed[i].demand_scenario, 'Solution time': models[i][1],
-                   'Gap': models[i][0].mipgap, 'weight violation': fixed[i].w_violation,
+                   'No. of vehicles': len(fixed[i].vehicles), 'Objective Value': models[i][0].objVal,
+                   'Vehicle capacity': fixed[i].vehicle_cap[0], 'Station capacity': fixed[i].station_cap[1],
+                   'Time Horizon': fixed[i].time_horizon, 'Demand scenario': fixed[i].demand_scenario,
+                   'Solution time': models[i][1], 'Gap': models[i][0].mipgap, 'weight violation': fixed[i].w_violation,
                    'weight deviation': fixed[i].w_dev_obj, 'weight reward': fixed[i].w_reward,
                    'reward weight dev': fixed[i].w_dev_reward, 'reward weight time': fixed[i].w_driving_time}
 
