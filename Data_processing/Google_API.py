b6a31ea22646d5b964385809be9ae3ce314b4b25
oyvorsh <oyvorsh@stud.ntnu.no>
2019-11-19 17:04:15 +0100

    Save info about each station in excel

--- Data_processing/Google_API.py
+++ Data_processing/Google_API.py
@@ -29,18 +29,29 @@ def get_address(origin_lat, origin_lon):
 
 
 def write_all_addresses():
-    df = pd.DataFrame(columns=['Station ID', 'Station name', 'Station Address', 'latitude', 'longitude'])
+    df = pd.DataFrame(columns=['Station ID', 'Station name', 'Station Address', 'latitude', 'longitude', 'init_B_bikes',
+                               'init_F_bikes', 'Scenario', 'Demand', 'Ideal State', 'B-bike-rate', 'F-bike-rate'])
 
     with open("station.json", 'r') as f:
         stations = json.load(f)
+
+    scenarios = ['A', 'B', 'C', 'D', 'E']
 
     for id, station in stations.items():
         longitude = float(station[0])
         latitude = float(station[1])
         address = get_address(latitude, longitude)
-        row = {'Station ID': id, 'Station name': address[0], 'Station Address': address[1], 'latitude': latitude,
-               'longitude': longitude}
-        df = df.append(row, ignore_index=True)
+        for scenario in scenarios:
+            init_battery_load = station[2][scenario][0]
+            init_flat_load = station[2][scenario][1]
+            incoming_battery_rate = station[2][scenario][2]
+            incoming_flat_rate = station[2][scenario][3]
+            demand = station[2][scenario][5]
+            row = {'Station ID': id, 'Station name': address[0], 'Station Address': address[1], 'latitude': latitude,
+                   'longitude': longitude, 'init_B_bikes': init_battery_load, 'init_F_bikes': init_flat_load,
+                   'Scenario': scenario, 'Demand': demand, 'Ideal State': 0, 'B-bike-rate': incoming_battery_rate,
+                   'F-bike-rate': incoming_flat_rate}
+            df = df.append(row, ignore_index=True)
     df.to_excel("../Output/stations.xlsx", sheet_name="Stations")
 
 
